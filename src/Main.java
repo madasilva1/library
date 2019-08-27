cf45189ae06be779265987e22e956f2b4edc08f2
marcos <madasilva195@gmail.com>
2019-08-26 22:48:19 -0400

    Frist commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.isBookBorowed();
+        boolean resulted = book.isBookBorowed();
+        System.out.println(resulted);
+
+    }
+}
